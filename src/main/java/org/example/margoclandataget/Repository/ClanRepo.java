e3da55673f6fe124b92a82670a03d1b46182deea
Cypis <lukdeb98@gmail.com>
2024-10-24 00:08:37 +0200

    Revert "add already existing functionality"
    This reverts commit e159d83abcd703f960ee20474b8b4e0134743e84.

--- src/main/java/org/example/margoclandataget/Repository/ClanRepo.java
+++
@@ -1,9 +0,0 @@
-package org.example.margoclandataget.Repository;
-
-import org.example.margoclandataget.Entity.ClanEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClanRepo extends JpaRepository<ClanEntity, Long>{
-
-    ClanEntity findByClanName(String clanName);
-}
